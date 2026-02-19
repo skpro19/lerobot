e245f4318f665be09e5cbd9f9805db0105d20fe6
skpro19 <shubhamkumarpro1@gmail.com>
2026-02-20 03:21:20 +0530

    add teleoperate_with_cameras.sh

---
+++ agenda.md
@@ -0,0 +1,4 @@
+## Feb 19 2026
+- dual camera setup
+- pick and place data collection using teleoperation
+- training + inference [1 round]
--- src/lerobot/bash/teleoperate_with_camera.sh
+++ src/lerobot/bash/teleoperate_with_camera.sh
@@ -3,10 +3,10 @@
 # Teleoperate command with USB camera configuration
 lerobot-teleoperate \
     --robot.type=so101_follower \
-    --robot.port=/dev/ttyACM0 \
-    --robot.cameras="{ front: {type: opencv, index_or_path: '/dev/video1', width: 1920, height: 1080, fps: 30, fourcc: 'MJPG'}}" \
+    --robot.port=/dev/ttyACM1 \
+    --robot.cameras="{ front: {type: opencv, index_or_path: '/dev/video1', width: 1920, height: 1080, fps: 30, fourcc: 'MJPG'}, c270: {type: opencv, index_or_path: '/dev/video5', width: 640, height: 480, fps: 30}}" \
     --robot.id=f_0 \
     --teleop.type=so101_leader \
-    --teleop.port=/dev/ttyACM1 \
+    --teleop.port=/dev/ttyACM0 \
     --teleop.id=l_0 \
     --display_data=true
